ad4bea5a2a2bdcc7127e035ef844abbf66902d03
Shilpa <shilpa.nivalagi@kalpitatechnologies.com>
2022-08-05 18:03:08 +0530

    interface eg

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿public interface Rocket
+{
+    public void flying();
+    
+}
+public interface Auto
+{
+    public void threeweel();
+}
+public class Flight : Rocket, Auto
+{
+    public void flying()
+    {
+        Console.WriteLine("Fly using wings");
+    }
+    public void threeweel()
+    {
+        Console.WriteLine("Run using three weels");
+    }
+
+}
+ //Explicit Inteface Implementation
+
+class Interface
+{
+    static void Main (string[] args)
+    {
+        Flight f = new Flight();
+
+        Rocket r = f;
+        r.flying();
+
+        Auto a = f;
+        a.threeweel();
+       
+    }
+}
